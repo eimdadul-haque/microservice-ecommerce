0186c57cbde146be09a90db2201564e47594693a
eimdadul_haque <eimdadulhaque@gmail.com>
2023-05-06 15:32:59 +0600

    base repository added

---
+++ src/microservice.ecommerce/Catalog.API/Repositories/BaseRepositotys/BaseRepositoty.cs
@@ -0,0 +1,50 @@
+﻿using Catalog.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.API.Repositories.BaseRepositotys
+{
+    public class BaseRepositoty<T> : IBaseRepositoty<T> where T : class
+    {
+        private CatalogDbContext _context;
+
+        public BaseRepositoty(CatalogDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            var entity = _context.Set<T>().Find(id);
+            if (entity != null)
+                _context.Set<T>().Remove(entity);
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetById(string id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<bool> Save()
+        {
+            if (await _context.SaveChangesAsync() > 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ src/microservice.ecommerce/Catalog.API/Repositories/BaseRepositotys/IBaseRepositoty.cs
@@ -0,0 +1,12 @@
+﻿namespace Catalog.API.Repositories.BaseRepositotys
+{
+    public interface IBaseRepositoty<T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(string id);
+        Task<T> GetById(string id);
+        Task<bool> Save();
+    }
+}
