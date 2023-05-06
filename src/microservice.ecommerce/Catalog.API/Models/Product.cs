fb6cead33843a34374e3aa5450962fb37224d7d8
eimdadul_haque <eimdadulhaque@gmail.com>
2023-05-06 15:32:22 +0600

    product entity added

---
+++ src/microservice.ecommerce/Catalog.API/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.API.Models.Auditentitys;
+
+namespace Catalog.API.Models
+{
+    public class Product : Auditentity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+        public string Summary { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
