96197ae40243e4e027788c979bb5bfd2c7c77521
eimdadul_haque <eimdadulhaque@gmail.com>
2023-05-06 15:30:57 +0600

    DbContext Added

---
+++ src/microservice.ecommerce/Catalog.API/Data/CatalogDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Catalog.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.API.Data
+{
+    public class CatalogDbContext : DbContext
+    {
+        public CatalogDbContext(DbContextOptions<CatalogDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
