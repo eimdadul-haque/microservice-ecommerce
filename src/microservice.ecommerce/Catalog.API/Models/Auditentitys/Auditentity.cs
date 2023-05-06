19a12bc0fdf4184e9adaa423ed276d1a7814531c
eimdadul_haque <eimdadulhaque@gmail.com>
2023-05-06 15:31:55 +0600

    Audit Identity added

---
+++ src/microservice.ecommerce/Catalog.API/Models/Auditentitys/Auditentity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.API.Models.Auditentitys
+{
+    public class Auditentity
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Modified { get; set; } = DateTime.Now;
+        public string CreatedBy { get; set; } = string.Empty;
+        public string ModifiedBy { get; set; } = string.Empty;
+        public string CreatedFrom { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+
+    }
+}
