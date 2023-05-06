cfe1b9795c2e70a6cf0ff76ab6ff88a4482e3b43
eimdadul_haque <eimdadulhaque@gmail.com>
2023-05-06 15:33:37 +0600

    product service added

---
+++ src/microservice.ecommerce/Catalog.API/Services/ProductService/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.API.Models;
+
+namespace Catalog.API.Services.ProductService
+{
+    public interface IProductService 
+    {
+        Task<Product> GetProductById(string Id);
+        Task<List<Product>> 
+    }
+}
---
+++ src/microservice.ecommerce/Catalog.API/Services/ProductService/ProductService.cs
@@ -0,0 +1,7 @@
+﻿namespace Catalog.API.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+
+    }
+}
